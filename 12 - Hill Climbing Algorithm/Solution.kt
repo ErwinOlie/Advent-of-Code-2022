95c70736214c0107cb3aa58768156c1336453831
Erwin Olie <erwin@pop-os.localdomain>
2023-01-06 16:00:16 +0100

    12 - Hill Climbing Algorithm

--- 12 - Hill Climbing Algorithm/Solution.kt
+++ 12 - Hill Climbing Algorithm/Solution.kt
@@ -1,5 +1,6 @@
 package nl.erwinolie.`Advent-of-Code-2022`.`12 - Hill Climbing Algorithm`
 
+import nl.erwinolie.extensions.Point2D
 import kotlin.streams.toList
 import nl.erwinolie.extensions.input
 
@@ -7,9 +8,9 @@ val map = input().lines()
     .map { it.chars().toList() }
     .toList()
 
-var start1 = Point(-1, -1)
-val starts2 = mutableListOf<Point>()
-var end = Point(-1, -1)
+var start1 = Point2D(-1, -1)
+val starts2 = mutableListOf<Point2D>()
+var end = Point2D(-1, -1)
 
 val heightMap = map.map { row ->
     row.map { charToHeight(it.toChar()) }.toList()
@@ -27,9 +28,9 @@ fun main() {
     for (y in map.indices) {
         for (x in map[y].indices) {
             when(map[y][x]) {
-                'S'.code -> start1 = Point(x, y)
-                'a'.code -> starts2.add(Point(x, y))
-                'E'.code -> end = Point(x, y)
+                'S'.code -> start1 = Point2D(x, y)
+                'a'.code -> starts2.add(Point2D(x, y))
+                'E'.code -> end = Point2D(x, y)
             }
         }
     }
@@ -41,20 +42,23 @@ fun main() {
     println(answer2)
 }
 
-fun findLowestDistance(end: Point, starts: List<Point>): Int {
-    val distances = mutableMapOf<Point, Int>()
-    val toVisit = mutableListOf<Point>()
+fun findLowestDistance(end: Point2D, starts: List<Point2D>): Int {
+    val distances = mutableMapOf<Point2D, Int>()
+    val toVisit = mutableListOf<Point2D>()
     for (y in heightMap.indices) {
         for (x in heightMap[y].indices) {
-            distances[Point(x, y)] = INFINITY
-            toVisit.add(Point(x, y))
+            distances[Point2D(x, y)] = INFINITY
+            toVisit.add(Point2D(x, y))
         }
     }
     distances[end] = 0
 
     while (toVisit.size > 0) {
         val closest = toVisit.sortedBy { distances[it] }.first()
-        closest.neighbourPoints().forEach {
+        closest.neighbours()
+            .filter { it.isInBoundedBoxInclusive(0, 0, map[0].size - 1L, map.size - 1L) }
+            .filter { heightMap[it.y.toInt()][it.x.toInt()] >= heightMap[closest.y.toInt()][closest.x.toInt()] - 1 }
+            .forEach {
             val newDistance = distances[closest]!! + 1
             if (newDistance < distances[it]!!) {
                 distances[it] = newDistance
@@ -67,12 +71,4 @@ fun findLowestDistance(end: Point, starts: List<Point>): Int {
         .filter { it.value >= 0}
         .filter { it.key in starts }
         .minOf { it.value }
-}
-
-data class Point(val x: Int, val y: Int) {
-    fun neighbourPoints() =
-        listOf(Point(x - 1, y), Point(x, y - 1), Point(x + 1, y), Point(x, y + 1))
-            .filter { it.x >= 0 && it.y >= 0 }
-            .filter { it.y < map.size && it.x < map[0].size }
-            .filter { heightMap[it.y][it.x] >= heightMap[this.y][this.x] - 1 }
 }
